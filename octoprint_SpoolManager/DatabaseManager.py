3c5f9ea154dc4b98ab546167b773a6435c02a1fb
Kuba Kotowicz <jkotowicz1@gmail.com>
2023-11-18 14:29:29 +0100

    added import psycopg2 back

--- octoprint_SpoolManager/DatabaseManager.py
+++ octoprint_SpoolManager/DatabaseManager.py
@@ -7,7 +7,7 @@ import os
 import logging
 import shutil
 import sqlite3
-import psycopg2
+#import psycopg2
 #import pymysql
 
 from octoprint_SpoolManager.WrappedLoggingHandler import WrappedLoggingHandler
